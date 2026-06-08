f328065617f4bdd6eca845b310a1513d55c914ac
irfanjul46 <irfantistm@gmail.com>
2026-06-08 12:34:17 +0700

    fix: selaraskan running session mlflow project

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -30,26 +30,24 @@ def main():
     
     # 2. Setup MLflow Tracking agar CI/CD mencatat hasil ke DagsHub
     # Pastikan environment variable MLFLOW_TRACKING_URI sudah diset di Workflow YAML kamu
-    mlflow.set_experiment("Football_Goals_Prediction_CI_CD")
+
     mlflow.sklearn.autolog() # Wajib untuk sinkronisasi dengan level Basic
     
     print("Memulai pelatihan model di CI/CD...")
-    
-    with mlflow.start_run(run_name="CI_CD_Automated_Training"):
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
-        model.fit(X_train, y_train)
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
         
-        # Dapatkan lokasi absolut dari file modelling.py saat ini
-        script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Dapatkan lokasi absolut dari file modelling.py saat ini
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     
     # Gabungkan dengan nama folder output agar tersimpan persis di sebelah file modelling.py
-        model_path = os.path.join(script_dir, "model_output")
-        if os.path.exists(model_path):
+    model_path = os.path.join(script_dir, "model_output")
+    if os.path.exists(model_path):
             shutil.rmtree(model_path)
         
-        # Simpan dalam format MLflow model
-        mlflow.sklearn.save_model(model, model_path)
-        print(f"✓ Model berhasil dilatih, di-log ke MLflow, dan disimpan ke {model_path}/")
+    # Simpan dalam format MLflow model
+    mlflow.sklearn.save_model(model, model_path)
+    print(f"✓ Model berhasil dilatih, di-log ke MLflow, dan disimpan ke {model_path}/")
 
 if __name__ == "__main__":
     main()
